cdc799844ac8c286ad638afb303eb47e1ee8c68a
dszarkiel <d.szarkiel@gmail.com>
2020-11-19 13:11:39 -0600

    commit tree 475e6870b5319ff6a4e2779344f45faa034f5c5b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,55 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get "/recipes" do
+    @recipes = Recipe.all
+
+    erb :index
+  end
+
+  get "/recipes/new" do
+    @recipe = Recipe.new
+
+    erb :new
+  end
+
+  post "/recipes" do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  #show 
+    get "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+
+    erb :show
+  end
+
+  #edit
+  get "/recipes/:id/edit" do
+    @recipe = Recipe.find(params[:id])
+
+    erb :edit
+  end
+
+  #update
+  patch "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = parans[:name]
+    @recipe.ingredients = parans[:ingredients]
+    @recipe.cook_time = parans[:cook_time]
+    @recipe.save
+    
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  #delete
+  delete "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+
+    redirect "/recipes"
+  end
 
 end
